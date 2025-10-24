3b88643d5b858d49cfe10d7fc8c83f7e44f76ba4
stunme <33939211+stunme@users.noreply.github.com>
2025-10-24 12:46:10 -0700

    Fix punctuation list for Chinese language handling
    cut by "，"，","，"、" will not split, if set to False

--- cosyvoice/utils/frontend_utils.py
+++ cosyvoice/utils/frontend_utils.py
@@ -76,11 +76,11 @@ def split_paragraph(text: str, tokenize, lang="zh", token_max_n=80, token_min_n=
             return len(tokenize(_text)) < merge_len
 
     if lang == "zh":
-        pounc = ['。', '？', '！', '；', '：', '、', '.', '?', '!', ';']
+        pounc = ['。', '？', '！', '；', '：','.', '?', '!', ';']
     else:
         pounc = ['.', '?', '!', ';', ':']
     if comma_split:
-        pounc.extend(['，', ','])
+        pounc.extend(['，', ',' ,'、' ])
 
     if text[-1] not in pounc:
         if lang == "zh":
